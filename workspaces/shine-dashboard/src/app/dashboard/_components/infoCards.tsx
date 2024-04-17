e5be34b0a3393d29807e0b47b6aeaa2cbf2a2b25
Mikael Marceniuk <mika.marceniuk@gmail.com>
2024-04-17 13:28:55 -0300

    feat(DashboardPage): Add 4 infoCards mocked

---
+++ workspaces/shine-dashboard/src/app/dashboard/_components/infoCards.tsx
@@ -0,0 +1,39 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { formatToCurrency } from "@/utils/formatter"
+
+interface IInfoCardProps {
+	title: string
+	content: {
+		value: number
+		type: 'INTEGER' | 'CENTS'
+		isTrendingUp: boolean
+		trendingPorcentage: string
+	}
+}
+
+const InfoCard: React.FC<IInfoCardProps> = ({ title, content }) => {
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>{title}</CardTitle>
+			</CardHeader>
+			<CardContent className='flex flex-col gap-1'>
+				<p className='font-bold text-2xl overflow-hidden text-ellipsis'>
+					{content.type == 'CENTS'
+						? formatToCurrency.format(content.value / 100)
+						: content.value
+					}
+				</p>
+				<div
+					data-istrending={content.isTrendingUp}
+					className='flex items-center gap-1 text-sm data-[istrending=true]:text-green-900 data-[istrending=false]:text-red-600'
+				>
+					<span>{content.trendingPorcentage}</span>
+					do que o mes passado
+				</div>
+			</CardContent>
+		</Card>
+	)
+}
+
+export default InfoCard
--- workspaces/shine-dashboard/src/app/dashboard/page.tsx
+++ workspaces/shine-dashboard/src/app/dashboard/page.tsx
@@ -1,7 +1,45 @@
+import InfoCard from "./_components/infoCards"
 
 const DashboardPage = () => {
 	return (
-		<>This is Dashboard Page</>
+		<div className='grid grid-cols-4 gap-4'>
+			<InfoCard
+				title='Faturamento no mes'
+				content={{
+					value: 192927,
+					type: 'CENTS',
+					isTrendingUp: true,
+					trendingPorcentage: '+16.8%'
+				}}
+			/>
+			<InfoCard
+				title='Vendas do mes'
+				content={{
+					value: 119,
+					type: 'INTEGER',
+					isTrendingUp: true,
+					trendingPorcentage: '+12%'
+				}}
+			/>
+			<InfoCard
+				title='Trafego no mes'
+				content={{
+					value: 256,
+					type: 'INTEGER',
+					isTrendingUp: false,
+					trendingPorcentage: '-1.3%'
+				}}
+			/>
+			<InfoCard
+				title='Novos clientes'
+				content={{
+					value: 8,
+					type: 'INTEGER',
+					isTrendingUp: true,
+					trendingPorcentage: '+1.2%'
+				}}
+			/>
+		</div>
 	)
 }
 
