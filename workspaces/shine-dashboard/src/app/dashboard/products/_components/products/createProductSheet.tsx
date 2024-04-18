db90c8acd4ec8ddf839f1c0b4db78f6ef3292825
Mikael Marceniuk <mika.marceniuk@gmail.com>
2024-04-18 17:54:33 -0300

    feat(ProductPage): Add createProductSheet

---
+++ workspaces/shine-dashboard/src/app/dashboard/products/_components/products/createProductSheet.tsx
@@ -0,0 +1,170 @@
+import { Button } from "@/components/ui/button"
+import { FileInput, FileUploader, FileUploaderContent, FileUploaderItem } from "@/components/ui/file-upload"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet"
+import { Textarea } from "@/components/ui/textarea"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { CloudUpload, Paperclip } from "lucide-react"
+import { useState } from "react"
+import { DropzoneOptions } from "react-dropzone"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const productSchema = z.object({
+	name: z.string(),
+	description: z.string().optional(),
+	image: z.array(z.instanceof(File)).max(8),
+	amount: z.coerce.number()
+})
+
+type ProductFormType = z.infer<typeof productSchema>
+
+const dropZoneConfig: DropzoneOptions = {
+	accept: {
+		"image/*": [".jpg", ".jpeg", ".png"],
+	},
+	multiple: true,
+	maxFiles: 8,
+	maxSize: 1 * 1024 * 1024,
+}
+
+const CreateProductSheet = () => {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const productForm = useForm<ProductFormType>({
+		resolver: zodResolver(productSchema),
+		defaultValues: {
+			name: undefined,
+			description: undefined,
+			image: [],
+			amount: undefined
+		}
+	})
+
+	const handleOnCancel = () => {
+		// productForm.reset()
+		setIsOpen(false)
+	}
+
+	const handleOnSubmit = async (data: ProductFormType) => {
+		console.log('createProductSheet/data: ', data)
+		await new Promise(res => setTimeout(res, 2000))
+	}
+
+	return (
+		<Sheet open={isOpen}>
+			<SheetTrigger asChild>
+				<Button onClick={() => setIsOpen(true)}>Criar novo produto</Button>
+			</SheetTrigger>
+			<SheetContent renderCloseIcon={false}>
+				<SheetHeader>
+					<SheetTitle>Criar um produto</SheetTitle>
+					<Separator />
+				</SheetHeader>
+
+				<Form {...productForm}>
+					<form className='mt-4 space-y-4' onSubmit={productForm.handleSubmit(handleOnSubmit)}>
+						<FormField
+							control={productForm.control}
+							name='name'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Nome (Obrigatorio)</FormLabel>
+									<FormControl>
+										<Input {...field} />
+									</FormControl>
+									<FormDescription>Nome do produto, estara visivel aos clientes.</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						<FormField
+							control={productForm.control}
+							name='description'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Descricao</FormLabel>
+									<FormControl>
+										<Textarea {...field} />
+									</FormControl>
+									<FormDescription>Aparece na pagina do produto e no pagamento.</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						<FormField
+							control={productForm.control}
+							name='image'
+							render={({ field: { value, onChange } }) => (
+								<FormItem>
+									<FormLabel>Imagens</FormLabel>
+									<FormControl>
+										<FileUploader
+											value={value}
+											onValueChange={onChange}
+											dropzoneOptions={dropZoneConfig}
+											className="relative bg-background rounded-lg p-2"
+										>
+											<FileInput className="outline-dashed outline-1 outline-black">
+												<div className="flex items-center justify-center flex-col py-4 w-full">
+													<CloudUpload />
+													<span>Clique para enviar ou arraste e solte aqui</span>
+												</div>
+											</FileInput>
+											<FileUploaderContent>
+												{value && value.length > 0 &&
+													value.map((file, i) => (
+														<FileUploaderItem key={i} index={i}>
+															<Paperclip className="h-4 w-4 stroke-current" />
+															<span>{file.name}</span>
+														</FileUploaderItem>
+													))}
+											</FileUploaderContent>
+										</FileUploader>
+									</FormControl>
+									<FormDescription>Aparece na pagina do produto e no pagamento.</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						<FormField
+							control={productForm.control}
+							name='amount'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Preco (Obrigatorio)</FormLabel>
+									<FormControl>
+										<Input type='number' {...field} />
+									</FormControl>
+									<FormDescription>Aparece na pagina do produto e no pagamento.</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						<Separator />
+						<SheetFooter>
+							<Button variant='outline' onClick={handleOnCancel}>Cancelar</Button>
+							<Button
+								type="submit"
+								disabled={productForm.formState.isSubmitting}
+							>
+								{productForm.formState.isSubmitting 
+									? 'Criando produto...'
+									: 'Criar produto'
+								}
+							</Button>
+						</SheetFooter>
+					</form>
+				</Form>
+			</SheetContent>
+		</Sheet>
+	)
+}
+
+export default CreateProductSheet
--- workspaces/shine-dashboard/src/app/dashboard/products/page.tsx
+++ workspaces/shine-dashboard/src/app/dashboard/products/page.tsx
@@ -1,18 +1,49 @@
+'use client'
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { useState } from "react"
+import CreateProductSheet from "./_components/products/createProductSheet"
 import ProductsPageContent from "./_components/products/productsPageContent"
 
+type AvailableTabsType = 'products' | 'coupons'
+
+const availableTabs = [
+	{
+		value: 'products',
+		label: 'Produtos',
+		content: <ProductsPageContent />
+	},
+	{
+		value: 'coupons',
+		label: 'Cupons',
+		content: <p>This is coupons page</p>
+	},
+] as const
+
 const ProductsPage = () => {
+	const [activeTab, setActiveTab] = useState<AvailableTabsType>('products')
+
 	return (
-		<div>
-			<Tabs defaultValue="products">
+		<>
+			<div className='flex justify-between items-center'>
+				<h1 className='font-bold text-2xl'>Catalogo de Produtos</h1>
+
+				{activeTab == "products" && <CreateProductSheet />}
+			</div>
+
+			<Tabs defaultValue="products" onValueChange={val => setActiveTab(val as AvailableTabsType)}>
 				<TabsList>
-					<TabsTrigger value="products">Produtos</TabsTrigger>
+					{availableTabs.map((tab, i) => (
+						<TabsTrigger key={i} value={tab.value}>{tab.label}</TabsTrigger>	
+					))}
 				</TabsList>
-				<TabsContent value="products">
-					<ProductsPageContent />
-				</TabsContent>
+				{availableTabs.map((tab, i) => (
+					<TabsContent key={i} value={tab.value}>
+						{tab.content}
+					</TabsContent>
+				))}
 			</Tabs>
-		</div>
+		</>
 	)
 }
 
