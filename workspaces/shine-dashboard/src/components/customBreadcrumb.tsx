525dd1620d8bbc5998c3039d518b5bbb3706c804
Mikael Marceniuk <mika.marceniuk@gmail.com>
2024-04-18 22:56:18 -0300

    fix(Breadcrumb): Solved warning of unique key and add href

--- workspaces/shine-dashboard/src/components/customBreadcrumb.tsx
+++ workspaces/shine-dashboard/src/components/customBreadcrumb.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
+import availablePages from "@/constants/availablePages"
 import { usePathname } from "next/navigation"
+import React from "react"
 
 const CustomBreadcrumb = () => {
 	const paths = usePathname().split('/').splice(1)	
@@ -9,16 +11,18 @@ const CustomBreadcrumb = () => {
 	return (
 		<Breadcrumb>
 			<BreadcrumbList>
-			{paths.map((path, i) =>(
-					// TODO Resolve Error of unique "key"
-					// TODO Add Href to BreadcrumbLink
-					<>
-						<BreadcrumbItem key={i}>
-							<BreadcrumbLink className='capitalize cursor-pointer'>{path}</BreadcrumbLink>
+			{paths.map((path, i) => {
+				const { hrefTo, name } = availablePages.find(pages => path == pages.path)!
+				
+				return (
+					<React.Fragment key={i}>
+						<BreadcrumbItem>
+							<BreadcrumbLink href={hrefTo} className='capitalize cursor-pointer'>{name}</BreadcrumbLink>
 						</BreadcrumbItem>
 						{i + 1 != paths.length && <BreadcrumbSeparator />}
-					</>
-				))}
+					</React.Fragment>
+				)
+			})}
 			</BreadcrumbList>
 		</Breadcrumb>
 	)
--- workspaces/shine-dashboard/src/constants/availablePages.tsx
+++ workspaces/shine-dashboard/src/constants/availablePages.tsx
@@ -1,33 +1,46 @@
 import { Home, LineChart, Package, ShoppingCart, Tag, Users } from 'lucide-react'
 
-const availablePages = [
+interface IAvaliablePage  {
+	hrefTo: string
+	path: string
+	icon: React.ReactNode,
+	name: string
+}
+
+const availablePages: IAvaliablePage[] = [
 	{
 		hrefTo: '/dashboard',
+		path: 'dashboard',
 		icon: <Home />,
 		name: 'Dashboard'
 	},
 	{
 		hrefTo: '/dashboard/orders',
+		path: 'orders',
 		icon: <ShoppingCart />,
 		name: 'Compras e Pedidos'
 	},
 	{
 		hrefTo: '/dashboard/products',
+		path: 'products',
 		icon: <Tag />,
-		name: 'Produtos'
+		name: 'Catalogo de Produtos'
 	},
 	{
 		hrefTo: '/dashboard/storage',
+		path: 'storage',
 		icon: <Package />,
 		name: 'Estoque'
 	},
 	{
 		hrefTo: '/dashboard/users',
+		path: 'users',
 		icon: <Users />,
 		name: 'Usuarios e Clientes'
 	},
 	{
 		hrefTo: '/dashboard/analytics',
+		path: 'analytics',
 		icon: <LineChart />,
 		name: 'Indicadores'
 	}
